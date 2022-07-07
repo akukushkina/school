0c764a81549d883419bcd1c0b3aca381e11ac009
akyukyushkina <akyukyushkina@gmail.com>
2022-07-07 22:42:50 +0300

    added animation

--- js/main.js
+++ js/main.js
@@ -21,23 +21,72 @@ scrollTracker.animate(
 
 // boy
 const animatedImages = document.querySelectorAll(".info__img");
-
+const triggerBottom = (window.innerHeight / 5 * 4);
 window.addEventListener("scroll", checkImages);
 
 checkImages();
 
 function checkImages() {
-  const triggerBottom = (window.innerHeight / 5) * 4;
+  
   animatedImages.forEach((image) => {
     const imageTop = image.getBoundingClientRect().top;
     if (imageTop < triggerBottom) {
       image.classList.add("show");
-    } else {
-      image.classList.remove("show");
-    }
+    } 
   });
 };
 
+// articles
+const article = document.querySelectorAll(".article");
 
+window.addEventListener("scroll", checkArticles);
+
+checkArticles();
+
+function checkArticles() {
+
+  article.forEach(article => {
+    const articleTop = article.getBoundingClientRect().top;
+    if (articleTop < triggerBottom) {
+      article.classList.add("scale");
+    } 
+  }) 
+}
+
+
+// comments
+const comments = document.querySelectorAll('.comment');
+
+window.addEventListener("scroll", checkComments);
+
+checkComments();
+
+function checkComments(){
+
+  comments.forEach(comment => {
+    const commentTop = comment.getBoundingClientRect().top;
+    if (commentTop < triggerBottom) {
+      comment.classList.add("slice");
+    } 
+  })
+}
+
+
+// members
+const members = document.querySelectorAll('.member');
+
+window.addEventListener("scroll", checkMembers);
+
+checkMembers();
+
+function checkMembers(){
+
+  members.forEach(member => {
+    const memberTop = member.getBoundingClientRect().top;
+    if (memberTop < triggerBottom) {
+      member.classList.add("slice");
+    } 
+  })
+}
 
 // check email
