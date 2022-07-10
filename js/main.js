3b2f3d8620820805d5e4d405ffee0b463d1bf7f4
akyukyushkina <akyukyushkina@gmail.com>
2022-07-10 11:47:54 +0300

    add adaptive to header

--- js/main.js
+++ js/main.js
@@ -21,20 +21,19 @@ scrollTracker.animate(
 
 // boy
 const animatedImages = document.querySelectorAll(".info__img");
-const triggerBottom = (window.innerHeight / 5 * 4);
+const triggerBottom = (window.innerHeight / 5) * 4;
 window.addEventListener("scroll", checkImages);
 
 checkImages();
 
 function checkImages() {
-  
   animatedImages.forEach((image) => {
     const imageTop = image.getBoundingClientRect().top;
     if (imageTop < triggerBottom) {
       image.classList.add("show");
-    } 
+    }
   });
-};
+}
 
 // articles
 const article = document.querySelectorAll(".article");
@@ -44,49 +43,52 @@ window.addEventListener("scroll", checkArticles);
 checkArticles();
 
 function checkArticles() {
-
-  article.forEach(article => {
+  article.forEach((article) => {
     const articleTop = article.getBoundingClientRect().top;
     if (articleTop < triggerBottom) {
       article.classList.add("scale");
-    } 
-  }) 
+    }
+  });
 }
 
-
 // comments
-const comments = document.querySelectorAll('.comment');
+const comments = document.querySelectorAll(".comment");
 
 window.addEventListener("scroll", checkComments);
 
 checkComments();
 
-function checkComments(){
-
-  comments.forEach(comment => {
+function checkComments() {
+  comments.forEach((comment) => {
     const commentTop = comment.getBoundingClientRect().top;
     if (commentTop < triggerBottom) {
       comment.classList.add("slice");
-    } 
-  })
+    }
+  });
 }
 
-
 // members
-const members = document.querySelectorAll('.member');
+const members = document.querySelectorAll(".member");
 
 window.addEventListener("scroll", checkMembers);
 
 checkMembers();
 
-function checkMembers(){
-
-  members.forEach(member => {
+function checkMembers() {
+  members.forEach((member) => {
     const memberTop = member.getBoundingClientRect().top;
     if (memberTop < triggerBottom) {
       member.classList.add("slice");
-    } 
-  })
+    }
+  });
+}
+
+// burger menu
+
+const burger__btn = document.querySelector(".burger__menu");
+const menu__list = document.querySelector(".menu__list");
+burger__btn.onclick = function () {
+  menu__list.classList.toggle("seen");
 }
 
 // check email
